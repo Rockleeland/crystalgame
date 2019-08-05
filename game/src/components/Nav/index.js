296a9c57e1b27ea76e3f615ede2565d3f6d82fd4
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-08-05 00:06:54 -0700

    changed score page route

--- game/src/components/Nav/index.js
+++ game/src/components/Nav/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {Navbar} from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar'
 import {NavLink} from 'react-router-dom';
 import AuthService from '../AuthService';
-import './style.css'
+import './style.css';
+
 class Nav extends React.Component {
  
 	constructor() {
@@ -15,39 +16,41 @@ class Nav extends React.Component {
 		if (this.Auth.loggedIn()) {
 			return(
 				<div>
-					<Navbar>
-	  					<Navbar.Brand >
-							<NavLink className='navLinks' to='/'> Home</NavLink>
-							<NavLink className='navLinks' to='/profile'> | Profile</NavLink>
-							<NavLink className='navLinks' to='/score'> | Score Board</NavLink>
-							<NavLink className='navLinks' to='/instructions'> | How to play</NavLink>
-							<NavLink className='navLinks' to='/game'> | New Game</NavLink>
-						</Navbar.Brand>
-	  					<Navbar.Toggle />
-	  					<Navbar.Collapse className="justify-content-end">
-						 
-           		            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-           		            <a className="nav-link" href="/" onClick={() => this.Auth.logout()}>Logout</a>
-           		       
-	    					<Navbar.Text>
-	     						Signed in as: <NavLink to='/profile'>{this.props.name}</NavLink>
-								 
-	    					</Navbar.Text>
-	  					</Navbar.Collapse>
+					<Navbar className='nav-container' expand="lg">
+						<Navbar.Brand >Mega Letters</Navbar.Brand>
+						<Navbar.Toggle aria-controls="basic-navbar-nav"/>
+						<Navbar.Collapse id="basic-navbar-nav">
+							<div className='mr-auto navbar-nav'>
+								<NavLink className='navLinks' to='/'>Home</NavLink>
+								<NavLink className='navLinks' to='/profile'>Profile</NavLink>
+								<NavLink className='navLinks' to='/score'>Score Board</NavLink>
+								<NavLink className='navLinks' to='/instructions'>How to play</NavLink>
+								<NavLink className='navLinks' to='/game'>New Game</NavLink>
+							</div>
+							   {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+							<div className='mr-auto navbar-nav'>
+								<Navbar.Text>Signed in as: <NavLink to='/profile'>{this.props.name}</NavLink></Navbar.Text>
+								<Navbar.Text><a className="nav-link" href="/" onClick={() => this.Auth.logout()}>Logout</a></Navbar.Text>
+							</div>
+						</Navbar.Collapse>
 					</Navbar>
 				</div>
 			)
 		} else {
 			return (
 				<div>
-					<Navbar>
-						<Navbar.Brand>
-							<NavLink className='navLinks' to='/'> Home</NavLink>
-							<NavLink className='navLinks' to='/instructions'> | How to play</NavLink>
-						</Navbar.Brand>
+					<Navbar className='nav-container' expand='lg'>
+						<Navbar.Brand>Mega Letters</Navbar.Brand>
+						<Navbar.Toggle aria-controls="basic-navbar-nav"/>
+						<Navbar.Collapse id="basic-navbar-nav">
+						<div className='mr-auto navbar-nav'>
+							<NavLink className='navLinks' to='/'>Home</NavLink>
+							<NavLink className='navLinks' to='/instructions'>How to play</NavLink>
+							</div>
 							<Navbar.Text>
-								Log In
+								<NavLink to="/login">Log In</NavLink>
 							</Navbar.Text>
+							</Navbar.Collapse>
 					</Navbar>
 				</div>
 			)
